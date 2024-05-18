89b631c960cefdf8430b0eb17536a6ac940600a3
Thomas BECHET <thomas.bechet.pro@gmail.com>
2024-05-18 08:22:26 +0200

    add patch compile commands script

---
+++ tools/patch_compile_commands.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+function usage {
+  echo "usage: $0 compile_commands_file"
+  exit 1
+}
+
+if [ $# -ne 1 ] || [ -z $1 ];
+then
+  usage 
+  exit
+fi
+
+# replace -I to -isystem (ensure import using brackets)
+sed -i 's/\-I/\-isystem/g' $1
+# no need to add NU_IMPLEMENTATION, it is in .clangd
